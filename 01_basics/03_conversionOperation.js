1743c31e9ba4becfba97b9e85fa3e6c39fb6d6a7
ANU-IT-dev <anupriaa30@gmail.com>
2026-06-26 14:52:10 +0000

    CONVERSION OF DATATYPES

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score ="30abc"let score ="30abc"   // it will give NaN means not a number
+
+ // let score =null  // it will give zero
+
+ // let score = undefined // it will give NaN
+
+ // let score = true // will give 1
+ // console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber =Number (score)
+//console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn =  1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 =>false
+// "" // empty string => false
+// "hitesh" =>true;
+
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
